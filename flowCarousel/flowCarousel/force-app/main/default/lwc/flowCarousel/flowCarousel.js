880235884c4ed29e43371eba37404cd7f220712b
jdayment <52637666+jdayment@users.noreply.github.com>
2021-03-18 12:05:09 -0700

    Add files via upload

---
+++ flowCarousel/flowCarousel/force-app/main/default/lwc/flowCarousel/flowCarousel.js
@@ -0,0 +1,30 @@
+import { LightningElement,api } from 'lwc';
+
+export default class FlowCarousel extends LightningElement {
+    @api firstImageURL;
+    @api firstHeader;
+    @api firstDescription;
+    @api firstAltText;
+    @api secondImageURL;
+    @api secondHeader;
+    @api secondDescription;
+    @api secondAltText;
+    @api thirdImageURL;
+    @api thirdHeader;
+    @api thirdDescription;
+    @api thirdAltText;
+    @api fourthImageURL;
+    @api fourthHeader;
+    @api fourthDescription;
+    @api fourthAltText;
+    @api fifthImageURL;
+    @api fifthHeader;
+    @api fifthDescription;
+    @api fifthAltText;
+    @api carouselTitle;
+
+
+
+
+
+}
