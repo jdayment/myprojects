5ed45df64af16bf0c5ca3bd55167c3ea25411f53
jdayment <52637666+jdayment@users.noreply.github.com>
2021-03-19 07:55:33 -0700

    Update flowCarousel.js

--- flowCarousel/flowCarousel/force-app/main/default/lwc/flowCarousel/flowCarousel.js
+++ flowCarousel/flowCarousel/force-app/main/default/lwc/flowCarousel/flowCarousel.js
@@ -1,30 +1,33 @@
-import { LightningElement,api } from 'lwc';
+import { LightningElement,api,track } from 'lwc';
 
 export default class FlowCarousel extends LightningElement {
-    @api firstImageURL;
-    @api firstHeader;
-    @api firstDescription;
-    @api firstAltText;
-    @api secondImageURL;
-    @api secondHeader;
-    @api secondDescription;
-    @api secondAltText;
-    @api thirdImageURL;
-    @api thirdHeader;
-    @api thirdDescription;
-    @api thirdAltText;
-    @api fourthImageURL;
-    @api fourthHeader;
-    @api fourthDescription;
-    @api fourthAltText;
-    @api fifthImageURL;
-    @api fifthHeader;
-    @api fifthDescription;
-    @api fifthAltText;
+    @api ImageURLs;
+    @api Headers;
+    @api Descriptions;
+    @api AltText;
     @api carouselTitle;
+    @track items = [];
 
-
-
-
-
+    connectedCallback() {
+        console.log("initializing FlowCarousel");
+        console.log("ImageURLs is:" + this.ImageURLs);
+        console.log("Descriptions is:" + this.Descriptions);
+        console.log("AltText is:" + this.AltText);
+        let items = [];
+        let index = 0;
+        console.log("Headers is: " + this.Headers);
+        
+        this.Headers.forEach(header => {
+          items.push({
+            header: header,
+            src: this.ImageURLs[index],
+            description: this.Descriptions[index],
+            alttext: this.AltText[index], 
+            uniqueId: this.AltText[index] + " + " + index     
+          });
+          console.log("items is: " + items);
+          index + 1;
+        });
+        this.items = items;
+    }
 }
